7f1a56f0dd6fabeb716d09b05dbe310592fb6803
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-16 18:09:47 +0600

    React

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const App = () => {
+
+    // State
+
+    // Auto Executed
+
+    // Arrow function => Event , Onchange, Logical Function, API Call
     return (
         <div>
+            <h1>Hello</h1>
 
         </div>
     );
