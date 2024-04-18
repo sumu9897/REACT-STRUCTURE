ed1afa300195464d6ea8684fd1d1468af0e0dc45
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-18 13:41:29 +0600

    Update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,16 +4,32 @@ const App = () => {
 
 
     //State
-    const [Number, SetNumber]=useState(15)
-    // cosnst Number=11
+    const [Number, SetNumber]=useState(0)
+    // const Number=11
 
     //View
+    const Plus=()=>{
+
+        SetNumber(Number+1)
+    }
+    const Minus=()=>{
+        SetNumber(Number-1)
+    }
 
 
     return (
         <div>
 
             {Number}
+            <button onClick={() => {
+                Plus()
+            }}>Plus
+            </button>
+            <button onClick={() => {
+                Minus()
+            }}>Minus
+            </button>
+
         </div>
     );
 };
